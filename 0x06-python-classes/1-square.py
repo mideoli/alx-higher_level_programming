e2570651a740d755e78febf89fe0e8fbce00d6cb
Miguel <dmiurgosmiguel@gmail.com>
2023-08-29 17:05:58 -0700

    fix: task 1 comment line

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,4 +3,5 @@ class Square:
     """Square class with private attribute"""
     def __init__(self, size):
         """Initialize data"""
-        self.__size = size """Private instatiation of size"""
+        self.__size = size
+        """Private instatiation of size"""
