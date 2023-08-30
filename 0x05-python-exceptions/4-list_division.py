8041e5b68dfccc8745b0c2cda28b608855e2815b
Miguel <dmiurgosmiguel@gmail.com>
2023-08-30 07:42:25 -0700

    add: task 4. Divide a list

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    division_results = []  # creates a list to store the divisionresults
+
+    for i in range(list_length):
+        try:
+            # attempt to perform the division
+            result = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            # handle specific exceptions that might occur
+            print("wrong tye")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0  # set result to 0 in case of error
+        finally:
+            # append the result to the list, regardless of erro
+            division_results.append(result)
+    return division_results  # return the list of  division results
