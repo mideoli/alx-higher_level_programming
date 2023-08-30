c6bcda6ae75c7a300792e2ab15f9994bba38a335
Miguel <dmiurgosmiguel@gmail.com>
2023-08-30 07:47:26 -0700

    fix: task 4 typo on output

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -8,7 +8,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
         except TypeError:
             # handle specific exceptions that might occur
-            print("wrong tye")
+            print("wrong type")
             result = 0
         except ZeroDivisionError:
             print("division by 0")
