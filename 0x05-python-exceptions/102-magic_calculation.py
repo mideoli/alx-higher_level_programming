2af7c82607f38c620c2b344792cd0a939c2a8797
Miguel <dmiurgosmiguel@gmail.com>
2023-08-30 09:38:13 -0700

    add: task 9. ByteCode -> Python #4

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    try:
+        result = 0
+        for i in range(1, 3):
+            if i > a:
+                raise ValueError('Too far')
+            else:
+                result += (a ** b) / i
+    except ValueError:
+        result = b + a
+    return result
