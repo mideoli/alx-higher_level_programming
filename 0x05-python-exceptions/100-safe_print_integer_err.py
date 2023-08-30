f98d4f2c3dc29b6f35b37c59e6ee870b3b2ab436
Miguel <dmiurgosmiguel@gmail.com>
2023-08-30 09:22:45 -0700

    fix: add TypeError Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,11 +3,11 @@ import sys
 
 
 def safe_print_integer_err(value):
+
     try:
         formatted_value = "{:d}".format(value)
         print(formatted_value)
         return True
-    except ValueError as ve:
-        error_message = "Exception: {}".format(ve)
-        print(error_message, file=sys.stderr)
+    except (TypeError, ValueError) as e:
+        sys.stderr.write("Exception: {}\n".format(e))
         return False
